315c695e81f3d938e58c7f5a100f2c76f91e9f79
havoriskova <ha.voriskova@gmail.com>
2022-12-06 20:21:18 +0100

    added bit of CSS
    - NavLink (= react-router-dom) is giving to the active nav className "active", so it's easy to use

--- 01-portfolio/src/App.js
+++ 01-portfolio/src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';    <img src={logo} className="App-logo" alt="logo" />
 import './App.css';
 import {NavLink, Route, BrowserRouter, Routes} from 'react-router-dom';
-//import {react, Component} from 'react';
 import About from './components/About.js';
 import Home from './components/Home.js';
 import Contact from './components/Contact.js';
@@ -9,25 +8,33 @@ import { Component } from 'react';
 
 class App extends Component {
   render() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-          <div>
-            <ul>
-              <li><NavLink to='/'>Home</NavLink></li>
-              <li><NavLink to='/about'>About</NavLink></li>
-              <li><NavLink to='/contact'>Contacts</NavLink></li>
-            </ul>
-          </div>
-          <Routes>
-            <Route exact path='/' element={<Home />} /> 
-            <Route path='/about' element={<About />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
+    return (
+      <BrowserRouter>
+        <div className="App">
+            <header>
+              <nav>
+                <ul>
+                  <li><NavLink to='/'>Home</NavLink></li>
+                  <li><NavLink to='/about'>About</NavLink></li>
+                  <li><NavLink to='/contact'>Contacts</NavLink></li>
+                </ul>
+              </nav>
+            </header>
+            
+            <main>
+              <Routes>
+                <Route exact path='/' element={<Home />} /> 
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+              </Routes>
+            </main>
 
-      </div>
-    </BrowserRouter>
-  );
+            <aside></aside>
+
+            <footer>Coded by Hana</footer>
+        </div>
+      </BrowserRouter>
+    );
 }}
 
 export default App;
