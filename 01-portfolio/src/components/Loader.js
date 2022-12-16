c7d5afd31691917b3aae998ec7afe8dcaa96729b
havoriskova <ha.voriskova@gmail.com>
2022-12-16 22:22:01 +0100

    update of Loader
    - refactoring
    - now it only needs two pictures with invisible bkg

--- 01-portfolio/src/components/Loader.js
+++ 01-portfolio/src/components/Loader.js
@@ -7,13 +7,12 @@ const Loader = (props) => {
 
     return(
         <div className="Loader container">
-           
             <div className="frame">
-                    <div className="center">
                         <img  src={loaderImg1} alt=""/>
                         <img  src={loaderImg2} alt="" />
-                        <div className="moving-thingie"><svg><circle r="120" cx="126" cy="126"/></svg></div>
-                    </div>
+                        <svg>
+                            <circle r="120" cx="126" cy="126"/>
+                        </svg>
             </div>
         </div>
     )
