a6456c856faf7188fef4352e08a96c616d3aa110
havoriskova <ha.voriskova@gmail.com>
2023-05-24 20:12:40 +0200

    Update Contact.js

--- 01-portfolio/src/components/Contact.js
+++ 01-portfolio/src/components/Contact.js
@@ -10,13 +10,13 @@ export default function Contact() {
             <div className="Contact container">
                 Contact !!! // little game would be nicer
 
-                    <form action="https://hanavoriskova.netlify.app/89f7a2105b81292e989a4eee19407380" method="POST">
+                    <form action="https://formsubmit.co/89f7a2105b81292e989a4eee19407380" method="POST">
                         <input type="hidden" name="_subject" value="New message from portfolio page!" />
                         <input type="text" name="name" required />
                         <input type="email" name="email" required />
                         <textarea name="text" required></textarea>
                         <input type="hidden" name="_captcha" value="false" />
-                        <input type="hidden" name="_next" value="https://hanavoriskova.netlify.app/thanks" />
+                        {/* <input type="hidden" name="_next" value="https://hanavoriskova.netlify.app/thanks" /> */}
                         <button type="submit">Send</button>
                         
                     </form>
