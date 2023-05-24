175109e109f92d74f316ecd0e4602e03e7c6938e
havoriskova <ha.voriskova@gmail.com>
2023-05-24 19:49:25 +0200

    Update Contact.js

--- 01-portfolio/src/components/Contact.js
+++ 01-portfolio/src/components/Contact.js
@@ -5,7 +5,7 @@ export default function Contact() {
         <div className="Contact container">
             Contact !!! // little game would be nicer
 
-                <form action="https://formsubmit.co/ha.voriskova@gmail.com" method="POST">
+                <form action="https://formsubmit.co/voriskova.ha@gmail.com" method="POST">
                     <input type="hidden" name="_subject" value="New message from portfolio page!" />
                     <input type="text" name="name" required />
                     <input type="email" name="email" required />
