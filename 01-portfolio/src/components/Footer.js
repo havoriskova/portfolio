7aa4071e73c707e265ab451b7ded71595b66e23a
havoriskova <ha.voriskova@gmail.com>
2023-05-26 22:43:26 +0200

    trying srcset

--- 01-portfolio/src/components/Footer.js
+++ 01-portfolio/src/components/Footer.js
@@ -1,6 +1,6 @@
 import "./Footer.css";
 import robots from '../img/robots_footer_pc.png';
-//import robot from '../img/robot_footer_phone.png';
+import robot from '../img/robot_footer_phone.png'; //pod 400px width
 
 
 const Footer = (props) => {
@@ -10,7 +10,9 @@ const Footer = (props) => {
         <>
 
             <div className="robots_container">
-                <img alt="robots" src={robots}/>
+                {/* <img alt="robots" src={robot} srcset={`${robot} 590w, ${robots} 1087w`} sizes="(max-width: 400px) 300px, 700px"/> */}
+                {/* <img alt="robots" src={robot} srcset={`${robot}, ${robots} 2x`}/> */}
+                <img alt="robots" src={robot} srcset={`${robot} 590w, ${robots} 1087w`}/>
             </div>
             <div className="hr"></div>
             <p>Coded by <a href="https://www.linkedin.com/in/hanavoriskova/?locale=en_US" rel="noreferrer" target='_blank'>Hana Voriskova</a>
