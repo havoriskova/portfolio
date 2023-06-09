93d41d85fbf668ab6d4b10d7cd8142bf2d1b1542
havoriskova <ha.voriskova@gmail.com>
2023-06-09 22:25:41 +0200

    added some content for projects part

--- 01-portfolio/src/components/projects/CSSChallenge.js
+++ 01-portfolio/src/components/projects/CSSChallenge.js
@@ -14,17 +14,71 @@ const CSSChallenge = (props) => {
                 <h3>React Webpage</h3> {/* mensi font, ale UPPERCASE */}
 
                 <div className='project_photo_phone_width'>
-                    <p>budouci fotka pro telefony</p>{/* <img alt='printscreen of page' src={}/> tady bude fotka optimalizovana pro telefon */}
+                    <div className="Blob frame">
+                        
+                        <div className="center">
+                            
+                                <div className="circle"></div>
+                                <div className="octagon"></div>
+                                <div className="triangle"></div>
+                                <div className="circle2"></div>
+                                
+                                <div className="kruh"></div>
+                                <div className="containery-pro-konfety k-octa">
+                                    <div className="container-pro-konfeta-o1"></div>
+                                    <div className="container-pro-konfeta-o2"></div>
+                                    <div className="container-pro-konfeta-o3"></div>
+                                    <div className="container-pro-konfeta-o4"></div>
+                                    <div className="container-pro-konfeta-o5"></div>
+                                    <div className="container-pro-konfeta-o6"></div>
+                                    <div className="container-pro-konfeta-o7"></div>
+                                    <div className="container-pro-konfeta-o8"></div>
+                                </div>
+                                <div className="containery-pro-konfety k-trouh">
+                                    <div className="container-pro-konfeta-t1"></div>
+                                    <div className="container-pro-konfeta-t2"></div>
+                                    <div className="container-pro-konfeta-t3"></div>
+                                </div>
+                        </div>
+
+                    </div>
                 </div>
 
-                <p>This responsive webpage is my gallery of CSS challenges made in React. I started the 100dayscss challenge in vanilla JS, but after some while I decided to redo the challenges in React.</p>
+                <p>After starting the 100dayscss challenge in vanilla JS, I decided to redesign some of the challenges in React. That's why I created this responsive website to help me track my progress.</p>
 
                     <a className='projectsLink' href='https://voriskova-css-challenge.netlify.app/' target='_blank' rel='noreferrer'>Visit website</a>
                     <a className='projectsLink' href='https://github.com/havoriskova/react-and-css-challenges' target='_blank' rel='noreferrer'><img className='projectsGithubLogo' src={githubLogo} alt='github logo'/>View code</a>
             </div>
             <div className="col-2">
                 {/*img printscreenu pro scroll */}
-               <p>budouci fotka pro desktop</p> {/* img optimalizovane pro pc */}
+                    <div className="Blob frame">
+            
+                        <div className="center">
+                            
+                                <div className="circle"></div>
+                                <div className="octagon"></div>
+                                <div className="triangle"></div>
+                                <div className="circle2"></div>
+                                
+                                <div className="kruh"></div>
+                                <div className="containery-pro-konfety k-octa">
+                                    <div className="container-pro-konfeta-o1"></div>
+                                    <div className="container-pro-konfeta-o2"></div>
+                                    <div className="container-pro-konfeta-o3"></div>
+                                    <div className="container-pro-konfeta-o4"></div>
+                                    <div className="container-pro-konfeta-o5"></div>
+                                    <div className="container-pro-konfeta-o6"></div>
+                                    <div className="container-pro-konfeta-o7"></div>
+                                    <div className="container-pro-konfeta-o8"></div>
+                                </div>
+                                <div className="containery-pro-konfety k-trouh">
+                                    <div className="container-pro-konfeta-t1"></div>
+                                    <div className="container-pro-konfeta-t2"></div>
+                                    <div className="container-pro-konfeta-t3"></div>
+                                </div>
+                        </div>
+        
+                    </div>
             </div>
             {/* <div className="projects-hr"></div> */}
         </div>
