46c10a2e39e0f97afa6c12fb7d150fa59f7e3b31
havoriskova <ha.voriskova@gmail.com>
2023-06-25 20:43:39 +0200

    Update Thanks.js

--- 01-portfolio/src/components/Thanks.js
+++ 01-portfolio/src/components/Thanks.js
@@ -5,8 +5,8 @@ const Thanks = (props) => {
 
     return(
         <div className="Thanks container">
-            <h1>Thank you, young Padawan, for submitting the form!</h1>
-            <p>May the Force be with you as we process your information faster than the Millennium Falcon completing the Kessel Run!</p>
+            <h1>Thank you for submitting the form!</h1>
+            <p>May the Force be with you as we process your information faster than the Millennium Falcon completing the Kessel Run.</p>
         </div>
     )
 };
